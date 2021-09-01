e19d5260abe81b05530dcc5e855c8f4ad69f56b3
kallard <kallard@student.21-school.ru>
2021-09-01 18:39:28 +0300

    Refactor code style

--- src/main/java/ru/format/lexer/ICommand.java
+++ src/main/java/ru/format/lexer/ICommand.java
@@ -1,7 +1,5 @@
 package ru.format.lexer;
 
-import ru.format.lexer.IContext;
-
 public interface ICommand {
     void execute(char ch, IContext context);
 }
--- src/main/java/ru/format/lexer/commands/AppendPostpone.java
+++ src/main/java/ru/format/lexer/commands/AppendPostpone.java
@@ -9,4 +9,3 @@ public class AppendPostpone implements ICommand {
         context.appendPostpone(ch);
     }
 }
-
